82f1fe20b97f5301eb4dc80d481dddf6b95a1da0
OA Wu <comdan66@gmail.com>
2016-01-15 17:20:15 +0800

    Finish works page.

---
+++ application/views/content/site/works/index/content.php
@@ -0,0 +1,101 @@
+<div class='_t'>
+  <span>設計作品</span>
+</div>
+
+<div class='b1'>
+  <div class='pics'>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+    <figure>
+      <a href=''>
+        <img src="http://www.zeusdesign.com.tw/resource/site/images/works/web.jpg" alt="網頁設計" style="opacity: 1;">
+      </a>
+
+      <figcaption>
+        <a href=''>網頁設計</a>
+        <p>EDM, 活動網站, 官網</p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <aside>
+    <a class='m'>網頁設計 • Web Design</a>
+      <a>官方網站設計</a>
+      <a>活動網站設計</a>
+      <a>Banner</a>
+      <a>EDM</a>
+      <a>UI & APP Design</a>
+      <a>RWD</a>
+    <a class='m'>平面設計 • Graphic Design</a>
+      <a>包裝設計</a>
+      <a>書籍設計</a>
+      <a>廣告設計</a>
+    <a class='m'>平面設計 • Graphic Design</a>
+      <a>包裝設計</a>
+      <a>書籍設計</a>
+      <a>廣告設計</a>
+    <a class='m'>商業攝影 • Photography</a>
+      <a>商品攝影</a>
+      <a>人像攝影</a>
+      <a>婚禮紀錄/自助婚紗</a>
+    <a>專案設計 • Design Project</a>
+  </aside>
+</div>
