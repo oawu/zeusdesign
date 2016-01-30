1095d2346948eded77981989144ee747e4cd2806
OA Wu <comdan66@gmail.com>
2016-01-30 20:03:19 +0800

    Fix code

--- application/views/content/site/works/show/content.php
+++ application/views/content/site/works/show/content.php
@@ -3,8 +3,9 @@
 </h1>
 
 <div class='b1'>
-  <article>
-    <section><p><?php echo $work->content;?></p></section>
+
+  <article> 
+    <!-- <section><p><?php echo $work->content;?></p></section> -->
     <div>
 <?php foreach ($work->blocks as $block) { ?>
         <section>
@@ -24,6 +25,11 @@
     </div>
   </article>
   <article>
+    <header>
+      <h2><a href='<?php echo base_url ('work', $work->site_show_page_last_uri ());?>'><?php echo $work->title;?></a></h2>
+      <div class="fb-like" data-href="<?php echo base_url ('work', $work->id);?>" data-send="false" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+    </header> 
+    <section><p><?php echo $work->content;?></p></section>
     <?php 
     foreach ($work->pictures as $picture) { ?>
       <figure>
