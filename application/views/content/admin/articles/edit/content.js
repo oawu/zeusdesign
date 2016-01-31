b05cc571fd280934aa22c8fd75f86b5cd0b95d02
OA Wu <comdan66@gmail.com>
2016-01-31 21:18:28 +0800

    Fix code

--- application/views/content/admin/articles/edit/content.js
+++ application/views/content/admin/articles/edit/content.js
@@ -4,17 +4,6 @@
  */
 
 $(function () {
-  CKEDITOR.on ('dialogDefinition', function (ev) {
-    var dialogName = ev.data.name;
-    var dialogDefinition = ev.data.definition;
-
-    if (dialogName == 'link') {
-      var infoTab = dialogDefinition.getContents ('info');
-      infoTab.remove ('linkType');
-      dialogDefinition.getContents ('target').get ('linkTargetType')['default'] = '_blank';
-    }
-  });
-
   $('textarea.cke').ckeditor ({
     filebrowserUploadUrl: $('#tools_ckeditors_upload_image_url').val (),
     height: 400,
