4be72806f5aef197d95234af65cee4640e663328
OA Wu <comdan66@gmail.com>
2016-02-03 23:22:38 +0800

    fix code.

--- application/config/setting/menu.php
+++ application/config/setting/menu.php
@@ -8,8 +8,8 @@
 $menu['admin'] = array (
     '管理後台' => array (
         '首頁' => array ('roles' => array ('member'), 'icon' => 'icon-home', 'href' => base_url ('admin'), 'class' => 'main', 'method' => 'index', 'target' => '_self'),
-        '訪客留言' => array ('roles' => array ('member'), 'icon' => 'icon-comment', 'href' => base_url ('admin', 'contacts'), 'class' => 'contacts', 'method' => '', 'target' => '_self'),
-        '使用者設定' => array ('roles' => array ('member'), 'icon' => 'icon-user2', 'href' => base_url ('admin', 'users'), 'class' => 'users', 'method' => '', 'target' => '_self'),
+        '訪客留言' => array ('roles' => array ('contact_manager'), 'icon' => 'icon-comment', 'href' => base_url ('admin', 'contacts'), 'class' => 'contacts', 'method' => '', 'target' => '_self'),
+        '使用者設定' => array ('roles' => array ('user_manager'), 'icon' => 'icon-user2', 'href' => base_url ('admin', 'users'), 'class' => 'users', 'method' => '', 'target' => '_self'),
       ),
     '部落格' => array (
         '文章分類' => array ('roles' => array ('bloger'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'article_tags'), 'class' => 'article_tags', 'method' => '', 'target' => '_self'),
@@ -17,11 +17,11 @@ $menu['admin'] = array (
           '上傳工具' => array ('roles' => array ('bloger'), 'icon' => 'icon-file-text2', 'href' => base_url ('admin', 'articles'), 'class' => 'tools', 'method' => '', 'target' => '_self', 'no_show' => true),
       ),
     '官網上搞' => array (
-        '作品分類' => array ('roles' => array ('member'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'work_tags'), 'class' => 'work_tags', 'method' => '', 'target' => '_self'),
-          '子分類' => array ('roles' => array ('member'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'work_tags'), 'class' => 'work_tag_tags', 'method' => '', 'target' => '_self', 'no_show' => true),
-        '作品管理' => array ('roles' => array ('member'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'works'), 'class' => 'works', 'method' => '', 'target' => '_self'),
-        'Banner管理' => array ('roles' => array ('member'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'banners'), 'class' => 'banners', 'method' => '', 'target' => '_self'),
-        'Promo管理' => array ('roles' => array ('member'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'promos'), 'class' => 'promos', 'method' => '', 'target' => '_self'),
+        '作品分類' => array ('roles' => array ('official_editor'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'work_tags'), 'class' => 'work_tags', 'method' => '', 'target' => '_self'),
+          '子分類' => array ('roles' => array ('official_editor'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'work_tags'), 'class' => 'work_tag_tags', 'method' => '', 'target' => '_self', 'no_show' => true),
+        '作品管理' => array ('roles' => array ('official_editor'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'works'), 'class' => 'works', 'method' => '', 'target' => '_self'),
+        'Banner管理' => array ('roles' => array ('official_editor'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'banners'), 'class' => 'banners', 'method' => '', 'target' => '_self'),
+        'Promo管理' => array ('roles' => array ('official_editor'), 'icon' => 'icon-images', 'href' => base_url ('admin', 'promos'), 'class' => 'promos', 'method' => '', 'target' => '_self'),
       ),
     '帳務' => array (
         '帳務類別' => array ('roles' => array ('invoice_generator'), 'icon' => 'icon-tags', 'href' => base_url ('admin', 'invoice_tags'), 'class' => 'invoice_tags', 'method' => '', 'target' => '_self'),
--- application/config/setting/role.php
+++ application/config/setting/role.php
@@ -5,23 +5,17 @@
  * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
-$role['roles'] = array (
-  'member',
-  'invoice_generator',
-  'bloger', 
-  'editor',
-  'templete_generator',
-  'user_manager',
-  'root'
-);
 $role['role_names'] = array (
     'member' => '登入後台',
     'invoice_generator' => '帳務上稿',
     'bloger' => '文章上稿',
-    'editor' => '官網上稿者',
+    'official_editor' => '官網上稿',
     'templete_generator' => '產生樣板',
-    'user_manager' => '會員管理員',
-    'root' => '權限最高者',
+    'user_manager' => '會員管理',
+    'contact_manager' => '留言管理',
+    'root' => '最高權限',
   );
+
+$role['roles'] = array_keys ($role['role_names']);
 
 $role['login_role'] = 'member';
